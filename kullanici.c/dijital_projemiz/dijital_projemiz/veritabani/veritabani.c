04d789aa369da17775f2241f569570a245cce727
aysunbuz000-debug <aysunbuz000@gmail.com>
2026-03-05 14:00:20 +0300

    altyapı ve veritabanı bağlantısı.

---
+++ kullanici.c/dijital_projemiz/dijital_projemiz/veritabani/veritabani.c
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dijital_projemiz.veritabani
+{
+    public class veritabani
+    {
+    private string baglantiYolu = "Server=localhost;Database=dijital_sql;Uid=root;Pwd=;";
+
+        public MySqlConnection BaglantiAl()
+        {
+            MySqlConnection baglanti = new MySqlConnection(baglantiYolu);
+            try
+            {
+                if (baglanti.State == System.Data.ConnectionState.Closed)
+                {
+                    baglanti.Open();
+                }
+                return baglanti;
+            }
+            catch (Exception ex)
+            {
+                // (Hata Yönetimi)
+                System.Windows.Forms.MessageBox.Show("Bağlantı Hatası: " + ex.Message);
+                return null;
+            }
+        }
+    }
+}
+    }
+}
